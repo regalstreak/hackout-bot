884a0a0bc44058eaaa1ca6f5a6cdbd22c933cedf
Neil Agarwal <neil@neilagarwal.me>
2020-10-10 19:51:56 +0530

    feat: simple ping-pong

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello world');
+import 'dotenv/config.js';
+import { Client } from 'discord.js';
+
+const client = new Client();
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (msg) => {
+	if (msg.content === 'ping') {
+		msg.reply('pong');
+	}
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
