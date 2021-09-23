9248f074304609cb5b97bf032c114991bea0627c
Neil Agarwal <neil@neilagarwal.me>
2021-09-23 20:05:39 +0530

    feat: rds

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -22,66 +22,39 @@ type TChannels = {
 	MEMES: TChannel[];
 };
 
-// HaXplore 2.0
-// rds = dev server channels, not needed on prod, only used for development
-export const CHANNELS: TChannels = {
-	REGISTRATION: [
-		{ name: 'register-for-hackathon', id: '824989242362822706' },
-		{ name: 'bots', id: '824975675013595136' },
-		{ name: 'rds-bots', id: '764467340078874639' },
-	],
-	DELETION: [
-		{ name: 'register-for-hackathon', id: '824989242362822706' },
-		// { name: 'rds-bots', id: '764467340078874639' },
-	],
-	LOGGER: [
-		{
-			name: 'registration-logger',
-			id: '824988589511016509',
-		},
-		// { name: 'rds-register-logger', id: '768476901806833694' },
-	],
-	MEMES: [
-		// {
-		// name: 'memes',
-		// id: '781575044702273577',
-		// },
-		// {
-		// 	name: 'rds-memes',
-		// 	id: '772218204826894337',
-		// },
-	],
-};
-
 // Hackout
 // export const CHANNELS: TChannels = {
 // 	REGISTRATION: [
 // 		{ name: 'register-hackathon', id: '765673553236066344' },
 // 		{ name: 'admin-bots', id: '764942500627611709' },
-// 		{ name: 'rds-bots', id: '764467340078874639' },
 // 	],
-// 	DELETION: [
-// 		{ name: 'register-hackathon', id: '765673553236066344' },
-// 		// { name: 'rds-bots', id: '764467340078874639' },
-// 	],
+// 	DELETION: [{ name: 'register-hackathon', id: '765673553236066344' }],
 // 	LOGGER: [
 // 		{
 // 			name: 'discord-register-logger',
 // 			id: '768465256699265054',
 // 		},
-// 		// { name: 'rds-register-logger', id: '768476901806833694' },
 // 	],
 // 	MEMES: [
 // 		{
 // 			name: 'memes',
 // 			id: '764985789607575633',
 // 		},
-// 		// {
-// 		// 	name: 'rds-memes',
-// 		// 	id: '772218204826894337',
-// 		// },
 // 	],
 // };
+
+// RDS
+export const CHANNELS: TChannels = {
+	REGISTRATION: [{ name: 'rds-bots', id: '764467340078874639' }],
+	DELETION: [{ name: 'rds-bots', id: '764467340078874639' }],
+	LOGGER: [{ name: 'rds-register-logger', id: '768476901806833694' }],
+	MEMES: [
+		{
+			name: 'rds-memes',
+			id: '772218204826894337',
+		},
+	],
+};
 
 export const REGISTRATION_FORMAT = `\n**${COMMANDS.REGISTER} Full Name email@domain.com**`;
 
@@ -98,31 +71,18 @@ export const REPLIES = {
 	UNREGISTER_FAIL: `You have not registered on ${HACKATHON_NAME} Discord, please register on Discord with the following format:${REGISTRATION_FORMAT}`,
 };
 
-// HaXplore 2.0
+// Hackout
 export const ROLES = {
 	EVERYONE: '@everyone',
-	HACKER_UNDER_REVIEW: 'Hacker 2021 (Under Review)',
-	HACKER_ACCEPTED: 'Hacker 2021',
-	// CONFERENCE: 'Conference 2020',
-	ORGANISER: 'Organizer',
-	ADMIN: 'Botmaster',
+	HACKER_UNDER_REVIEW: 'Hacker 2020 (Under Review)',
+	HACKER_ACCEPTED: 'Hacker 2020',
+	CONFERENCE: 'Conference 2020',
+	ORGANISER: 'Organiser',
+	ADMIN: 'Server Admin',
 	MENTOR: 'Mentor',
-	SPEAKER: 'Judge',
-	FINDING_TEAMS: 'Solo Team',
+	SPEAKER: 'Speaker',
+	FINDING_TEAMS: 'Finding Teams',
 };
-
-// Hackout
-// export const ROLES = {
-// 	EVERYONE: '@everyone',
-// 	HACKER_UNDER_REVIEW: 'Hacker 2020 (Under Review)',
-// 	HACKER_ACCEPTED: 'Hacker 2020',
-// 	CONFERENCE: 'Conference 2020',
-// 	ORGANISER: 'Organiser',
-// 	ADMIN: 'Server Admin',
-// 	MENTOR: 'Mentor',
-// 	SPEAKER: 'Speaker',
-// 	FINDING_TEAMS: 'Finding Teams',
-// };
 
 export const DEVFOLIO_CSV = {
 	NO_TEAM: 'N/A',
